514043f3983b015558c0ecfb958dc75daa62fcfe
unknown <kilaci@kilaci-asus.UNITSOFT.local>
2020-10-08 16:01:14 +0200

    #4: TaskType Equals tőrlés

--- webdimension.Data/Model/TaskType.cs
+++ webdimension.Data/Model/TaskType.cs
@@ -7,36 +7,7 @@ namespace webdimension.Data.Model
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as TaskType);
-        }
-
-        public bool Equals (TaskType tasktype)
-        {
-            if (null==tasktype)
-            {
-                return false;
-            }
-
-            if  (Id!=tasktype.Id || Name!=tasktype.Name)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {    
-                var hash = 27;
-                hash = (13 * hash) + Id.GetHashCode();
-                hash = (13 * hash) + Name.GetHashCode();
-                return hash;
-            }
-        }
+        
 
     }
 }
